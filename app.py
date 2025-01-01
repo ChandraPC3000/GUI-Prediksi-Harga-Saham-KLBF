6f6708db81ffcff152120046fc4cc4c3c163210b
Chandra Putra Ciptaningtyas <90868659+ChandraPC3000@users.noreply.github.com>
2025-01-02 02:28:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,15 @@ st.markdown("""
 .left {
     text-align: left;
     margin-left: 50px;
+    margin-top: 20px;
 }
 .right {
     text-align: right;
     margin-right: 50px;
+    margin-top: 20px;
+}
+.space-below {
+    margin-bottom: 5px;
 }
 </style>
 """, unsafe_allow_html=True)
@@ -32,15 +37,16 @@ with col3:
 
 # Title and information
 st.markdown('<h1 class="center">Prediksi Harga Saham PT Kalbe Farma Tbk (KLBF) Menggunakan Metode XGBoost dan LSTM</h1>', unsafe_allow_html=True)
-st.markdown('<p class="center">Oleh:</p>', unsafe_allow_html=True)
-st.markdown('<p class="center">Chandra Putra Ciptaningtyas</p>', unsafe_allow_html=True)
+st.markdown('<p class="center space-below">Oleh:</p>', unsafe_allow_html=True)
+st.markdown('<p class="center space-below"><b>Chandra Putra Ciptaningtyas</b></p>', unsafe_allow_html=True)
 st.markdown('<p class="center">NIM: 123456789</p>', unsafe_allow_html=True)
 
 # Display dosen pembimbing information
 col_left, col_right = st.columns([1, 1])
 
 with col_left:
-    st.markdown('<p class="left"><b>Dosen Pembimbing 1:</b><br>Dr. Dosen Pembimbing Satu, S.T., M.T.<br><i>NIP: 12345678</i></p>', unsafe_allow_html=True)
+    st.markdown('<p class="left"><b>Dosen Pembimbing 1:</b><br>Dr. Triastuti Wuryandari, S.Si., M.Si.<br><i>NIP. 197109061998032001</i></p>', unsafe_allow_html=True)
 
 with col_right:
-    st.markdown('<p class="right"><b>Dosen Pembimbing 2:</b><br>Prof. Dosen Pembimbing Dua, Ph.D.<br><i>NIP: 87654321</i></p>', unsafe_allow_html=True)
+    st.markdown('<p class="right"><b>Dosen Pembimbing 2:</b><br>Miftahul Jannah, S.Si., M.Si.<br><i>NIP. H.7.199804242023072001
+</i></p>', unsafe_allow_html=True)
