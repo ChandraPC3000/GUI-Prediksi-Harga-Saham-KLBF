e057051b93ab58e708ef79cabfe6460188c7af07
Chandra Putra Ciptaningtyas <90868659+ChandraPC3000@users.noreply.github.com>
2025-01-02 18:39:08 +0700

    Update Beranda.py

--- pages/Beranda.py
+++ pages/Beranda.py
@@ -25,6 +25,9 @@ st.markdown("""
 .space-between {
     margin-top: 20px; /* Space between NIM and dosen pembimbing */
 }
+.extra-space {
+    margin-bottom: 10px; /* Space between title and name */
+}
 </style>
 """, unsafe_allow_html=True)
 
@@ -53,7 +56,11 @@ st.markdown('<div class="space-between"></div>', unsafe_allow_html=True)
 col_left, col_right = st.columns([1, 1])
 
 with col_left:
-    st.markdown('<p class="adjusted-left"><b>Dosen Pembimbing 1:</b><br>Dr. Triastuti Wuryandari, S.Si., M.Si.<br>NIP. 197109061998032001</p>', unsafe_allow_html=True)
+    st.markdown('<p class="adjusted-left"><b>Dosen Pembimbing 1:</b></p>', unsafe_allow_html=True)
+    st.markdown('<div class="extra-space"></div>', unsafe_allow_html=True)
+    st.markdown('<p class="adjusted-left">Dr. Triastuti Wuryandari, S.Si., M.Si.<br>NIP. 197109061998032001</p>', unsafe_allow_html=True)
 
 with col_right:
-    st.markdown('<p class="adjusted-right"><b>Dosen Pembimbing 2:</b><br>Miftahul Jannah, S.Si., M.Si.<br>NIP. H.7.199804242023072001</p>', unsafe_allow_html=True)
+    st.markdown('<p class="adjusted-right"><b>Dosen Pembimbing 2:</b></p>', unsafe_allow_html=True)
+    st.markdown('<div class="extra-space"></div>', unsafe_allow_html=True)
+    st.markdown('<p class="adjusted-right">Miftahul Jannah, S.Si., M.Si.<br>NIP. H.7.199804242023072001</p>', unsafe_allow_html=True)
